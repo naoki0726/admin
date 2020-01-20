bcc1560e571c3f152ded1c1f570ade4f47a169f0
naoki0726 <59826204+naoki0726@users.noreply.github.com>
2020-01-20 12:42:22 +0900

    Add files via upload
    Django超入門　４章のコード
    
    宿題

--- views.py
+++ views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import redirect
-from .models import Friend
-from .forms import FriendForm
+from .models import Friend, Message
+from .forms import FriendForm, MessageForm
 from .forms import FindForm
 from django.db.models import Q
+from django.db.models import Count,Sum,Avg,Min,Max
+from .forms import CheckForm
+from django.core.paginator import Paginator
 
 
-def index(request):
+def index(request, num=1):
     data = Friend.objects.all()
+    page = Paginator(data, 3)
     params = {
             'title': 'Hello',
-            'data': data,
+            'message': '',
+            'data': page.get_page(num),
             }
     return render(request, 'hello/index.html', params)
 
@@ -54,11 +59,13 @@ def delete(request, num):
 
 def find(request):
     if (request.method == 'POST'):
-        msg = 'search result:'
+        msg = request.POST['find']
         form = FindForm(request.POST)
-        str = request.POST['find']
-        list = str.split()
-        data = Friend.objects.filter(name__in=list)
+        sql = 'select * from hello_friend'
+        if (msg != ''):
+            sql += ' where ' + msg
+        data = Friend.objects.raw(sql)
+        msg = sql
     else:
         msg = 'search words...'
         form = FindForm()
@@ -70,3 +77,33 @@ def find(request):
             'data': data,
             }
     return render(request, 'hello/find.html', params)
+
+def check(request):
+    params = {
+            'title': 'Hello',
+            'message': 'check validation.',
+            'form': FriendForm(),
+            }
+    if (request.method == 'POST'):
+        obj = Friend()
+        form = FriendForm(request.POST, instance=obj)
+        params['form'] = form
+        if (form.is_valid()):
+            params['message'] = 'OK!'
+        else:
+            params['message'] = 'no good.'
+    return render(request, 'hello/check.html', params)
+
+def message(request, page=1):
+    if (request.method == 'POST'):
+        obj = Message()
+        form = MessageForm(request.POST, instance=obj)
+        form.save()
+    data = Message.objects.all().reverse()
+    paginator = Paginator(data, 5)
+    params = {
+            'title': 'Message',
+            'form': MessageForm(),
+            'data': paginator.get_page(page),
+            }
+    return render(request, 'hello/message.html', params)
